45b9476d003fa9b0e2f863f5cc1ca5c5adebc462
loubinfeng <loubinfeng2013@163.com>
2021-03-10 16:34:04 +0800

    增加工具模块，classutils

--- entry/src/main/java/com/lbf/harmonytools/MyApplication.java
+++ entry/src/main/java/com/lbf/harmonytools/MyApplication.java
@@ -1,10 +1,23 @@
 package com.lbf.harmonytools;
 
+import com.lbf.utils.ClassUtils;
 import ohos.aafwk.ability.AbilityPackage;
+
+import java.util.List;
 
 public class MyApplication extends AbilityPackage {
     @Override
     public void onInitialize() {
         super.onInitialize();
+
+        try {
+            List<Class<?>> classList = ClassUtils.ScanClassInfoWithPackageName("com.lbf.harmonytools.slice", this);
+            for (Class clz : classList){
+                System.out.println("clz="+clz.getCanonicalName());
+            }
+        } catch (Exception e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
     }
 }
